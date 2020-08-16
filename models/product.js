9bcfa27ea7c775d6dfc3608dc56c1a94e64a43f6
Nneko Branche <4615594+nneko@users.noreply.github.com>
2020-08-16 14:36:16 -0500

    Product model

--- models/product.js
+++ models/product.js
@@ -2,13 +2,13 @@ const cfg = require('../configuration')
 const db = require('../adapters/storage/'+cfg.dbAdapter)
 const validator = require('../utilities/validator')
 const debug = cfg.env == 'development' ? true : false
-const user = require('user')
+const shop = require('shop')
 
-let shop = {}
+let product = {}
 
-shop.isValid = (s) => {
+product.isValid = (p) => {
     try {
-        if (validator.isNotNull(p) && typeof (p.shop) == 'string' && await shop.exists({ _id: db.getObjectId(p.shop) }) && typeof (p.name) == 'string' && validator.isNotNull(p.name) && typeof (p.description) == 'string' && validator.isNotNull(p.description) && s.hasOwnProperty('address') && s.hasOwnProperty('phoneNumber') && p.hasOwnProperty('parentProduct')) {
+        if (validator.isNotNull(p) && typeof (p.shop) == 'string' && await shop.exists({ _id: db.getObjectId(p.shop) }) && typeof (p.name) == 'string' && validator.isNotNull(s.name) && typeof (s.description) == 'string' && validator.isNotNull(s.description) && p.hasOwnProperty('image') && p.hasOwnProperty('specification') && p.hasOwnProperty('price') && p.hasOwnProperty('currecy') && p.hasOwnProperty('isSKU') && p.hasOwnProperty('SKU')) {
             return true
         } else {
             return false
@@ -19,13 +19,13 @@ shop.isValid = (s) => {
     }
 }
 
-shop.exists = async (s) => {
+product.exists = async (p) => {
     try {
-        const result = validator.isNotNull(await shop.read(s,{limit: 1}))
+        const result = validator.isNotNull(await product.read(p,{limit: 1}))
         if(debug) {
-            console.log('Checking shop exits: ')
-            console.log(s)
-            console.log('Shop exists ' + result)
+            console.log('Checking product exits: ')
+            console.log(p)
+            console.log('Product exists ' + result)
         }
         return result
     } catch (e) {
@@ -34,26 +34,26 @@ shop.exists = async (s) => {
     }
 }
 
-shop.create = (s) => {
+product.create = (s) => {
     return new Promise(async (resolve,reject) => {
         try {
-            if(!shop.isValid(s)){
-                let e = new Error('Invalid shop object')
-                e.name = 'ShopError'
+            if(!product.isValid(p)){
+                let e = new Error('Invalid product object')
+                e.name = 'ProductError'
                 e.type = 'Invalid'
                 throw e
             } 
             
-            if (await shop.exists({ name: s.name })) {
-                let e = new Error('Shop already exists')
-                e.name = 'ShopError'
+            if (await product.exists({ name: p.name, shop: p.shop })) {
+                let e = new Error('Product already exists')
+                e.name = 'ProductError'
                 e.type = 'Duplicate'
                 throw e
             }
 
-            s.name = s.name.toLowerCase()
+            p.name = p.name.toLowerCase()
 
-            const result = await db.get().collection('shops').insertOne(s)
+            const result = await db.get().collection('products').insertOne(p)
             resolve(result)
         } catch (e) {
             reject(e)
@@ -61,41 +61,41 @@ shop.create = (s) => {
     })
 }
 
-shop.read = (properties, options) => {
+product.read = (properties, options) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const shopCollection = db.get().collection('shops')
+            const productCollection = db.get().collection('products')
             if(validator.isNotNull(options)) {
                 if(typeof(options.limit) !== 'undefined') {
                     switch(options.limit) {
                         case 1:
-                            const result = await shopCollection.findOne(properties)
+                            const result = await productCollection.findOne(properties)
                             resolve(result)
                             break
                         default:
-                            const cursor = await shopCollection.find(properties).limit(options.limit)
+                            const cursor = await productCollection.find(properties).limit(options.limit)
                             resolve(cursor.toArray())
                             break
                     }
                 } else if(typeof(options.findBy) !== 'undefined') {
                     switch(options.findBy){
                         case 'id':
-                            const result = await shopCollection.findOne({'_id': db.getObjectId(properties)})
+                            const result = await productCollection.findOne({'_id': db.getObjectId(properties)})
                             resolve(result)
                             break
                         default:
-                            const cursor = await shopCollection.find(properties).limit(1)
+                            const cursor = await productCollection.find(properties).limit(1)
                             resolve(cursor.toArray())
                             break
                     }
                 } else {
                     let e = new Error('Invalid options to find operator')
-                    e.name = 'ShopError'
+                    e.name = 'ProductError'
                     e.type = 'Find Operation'
                     throw e
                 }
             } else {
-                const cursor = await shopCollection.find(properties)
+                const cursor = await productCollection.find(properties)
                 resolve(cursor.toArray())
             }
         } catch (e) {
@@ -104,7 +104,7 @@ shop.read = (properties, options) => {
     })
 }
 
-shop.update = (filters, values, options, operator) => {
+product.update = (filters, values, options, operator) => {
     return new Promise(async (resolve, reject) => {
         try {
             let opr = '$set'
@@ -119,16 +119,16 @@ shop.update = (filters, values, options, operator) => {
                     break
                 default:
                     if (validator.isNotNull(operator)) {
-                        let e = new Error('Invalid shop operation')
-                        e.name = 'ShopError'
+                        let e = new Error('Invalid product operation')
+                        e.name = 'ProductError'
                         e.type = 'Invalid Operation'
                         throw e
                     }
             }
-            const shopCollection = db.get().collection('shops')
+            const productCollection = db.get().collection('products')
             let operation = {}
             operation[opr] = values
-            const result = await shopCollection.updateMany(filters,operation,options)
+            const result = await productCollection.updateMany(filters,operation,options)
             resolve(result)
         } catch (e) {
             reject(e)
@@ -136,11 +136,11 @@ shop.update = (filters, values, options, operator) => {
     })
 }
 
-shop.delete = (filters) => {
+product.delete = (filters) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const shopCollection = db.get().collection('shops')
-            const result = await shopCollection.deleteMany(filters)
+            const productCollection = db.get().collection('products')
+            const result = await productCollection.deleteMany(filters)
             resolve(result)
         } catch (e) {
             reject(e)
@@ -148,4 +148,4 @@ shop.delete = (filters) => {
     })
 }
 
-module.exports = shop
+module.exports = product
--- models/schema.md
+++ models/schema.md
@@ -2,13 +2,13 @@ Currency:
 Fields - id, code
 
 Product:
-Fields - _id, shopID, parentProductID, productName, description, specifications, isSKU, SKU, price, currency, productImage
+Fields - _id, shop, parentProduct, name, description, specifications, isSKU, SKU, price, currency, image
 
 Catalog:
 Fields - _id, name, description, productListing: []
 
 Order:
-Fields - _id, timestamp, userID, productId, quantity
+Fields - _id, timestamp, user, product, quantity
 
 Shop:
 Fields - _id, owner, name, description, address: {streetAddress, locality, region, postalCode, country}, phoneNumber: {value,type}

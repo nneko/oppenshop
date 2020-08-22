dfdbc8c76236c6772814a93aee9ba0c66d5d5e4e
Nneko Branche <4615594+nneko@users.noreply.github.com>
2020-08-22 17:18:53 -0500

    Basic validator tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,10 +1,26 @@
-const { user } = require('../models/user')
-const { validator } = require('../utilities/validator')
+const validator = require('../utilities/validator')
+const usermodel = require('../models/user')
 
-test('validate undefined object is null', () => {
+test('validator isNotNull undefined object returns false', () => {
     expect(validator.isNotNull(undefined)).toBe(false)
 })
 
-test('create a new local user account', () => {
+test('validator isNotNull empty string object returns false', () => {
+    expect(validator.isNotNull("")).toBe(false)
+})
 
+test('validator isNotNull null value returns false', () => {
+    expect(validator.isNotNull(null)).toBe(false)
+})
+
+test('validator isNotNull object returns true', () => {
+    expect(validator.isNotNull({})).toBe(true)
+})
+
+test('validator isEmailAddress check on plain string without @ returns false', () => {
+    expect(validator.isEmailAddress('test email with @')).toBe(false)
+})
+
+test('validator isEmailAddress check for standard email address returns true', () => {
+    expect(validator.isEmailAddress('test@example.com')).toBe(true)
 })

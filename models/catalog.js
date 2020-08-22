e5df5b5419f131229044b93013b7f4d384c7a83c
Nneko Branche <4615594+nneko@users.noreply.github.com>
2020-08-22 15:16:54 -0500

    Catalog schema description

---
+++ models/catalog.js
@@ -0,0 +1,162 @@
+const cfg = require('../configuration')
+const db = require('../adapters/storage/'+cfg.dbAdapter)
+const validator = require('../utilities/validator')
+const debug = cfg.env == 'development' ? true : false
+const shop = require('./shop')
+
+let product = {}
+
+product.isValid = async (p) => {
+    try {
+        //If product is associated with shop validate that shop exists
+        if(p.shop) {
+            let shopExists = await shop.exists({ _id: db.getObjectId(p.shop) })
+            if(!shopExists) {
+                let e = new Error('Cannot create product using invalid shop')
+                e.name = 'ProductError'
+                e.type = 'Invalid Shop'
+                throw e
+            }
+        }
+
+        if (validator.isNotNull(p) && typeof (p.shop) == 'string' && typeof (p.name) == 'string' && validator.isNotNull(s.name) && typeof (s.description) == 'string' && validator.isNotNull(s.description) && p.hasOwnProperty('image') && p.hasOwnProperty('specification') && p.hasOwnProperty('price') && p.hasOwnProperty('currecy') && p.hasOwnProperty('isSKU') && p.hasOwnProperty('SKU')) {
+            return true
+        } else {
+            return false
+        }
+    } catch (e) {
+        console.error(e)
+        throw e
+    }
+}
+
+product.exists = async (p) => {
+    try {
+        const result = validator.isNotNull(await product.read(p,{limit: 1}))
+        if(debug) {
+            console.log('Checking product exits: ')
+            console.log(p)
+            console.log('Product exists ' + result)
+        }
+        return result
+    } catch (e) {
+        if(debug) console.log(e)
+        throw new Error('Unable to query database')
+    }
+}
+
+product.create = (p) => {
+    return new Promise(async (resolve,reject) => {
+        try {
+            if(!product.isValid(p)){
+                let e = new Error('Invalid product object')
+                e.name = 'ProductError'
+                e.type = 'Invalid'
+                throw e
+            }
+
+            if (await product.exists({ name: p.name, shop: p.shop })) {
+                let e = new Error('Product already exists')
+                e.name = 'ProductError'
+                e.type = 'Duplicate'
+                throw e
+            }
+
+            p.name = p.name.toLowerCase()
+
+            const result = await db.get().collection('products').insertOne(p)
+            resolve(result)
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+product.read = (properties, options) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const productCollection = db.get().collection('products')
+            if(validator.isNotNull(options)) {
+                if(typeof(options.limit) !== 'undefined') {
+                    switch(options.limit) {
+                        case 1:
+                            const result = await productCollection.findOne(properties)
+                            resolve(result)
+                            break
+                        default:
+                            const cursor = await productCollection.find(properties).limit(options.limit)
+                            resolve(cursor.toArray())
+                            break
+                    }
+                } else if(typeof(options.findBy) !== 'undefined') {
+                    switch(options.findBy){
+                        case 'id':
+                            const result = await productCollection.findOne({'_id': db.getObjectId(properties)})
+                            resolve(result)
+                            break
+                        default:
+                            const cursor = await productCollection.find(properties).limit(1)
+                            resolve(cursor.toArray())
+                            break
+                    }
+                } else {
+                    let e = new Error('Invalid options to find operator')
+                    e.name = 'ProductError'
+                    e.type = 'Find Operation'
+                    throw e
+                }
+            } else {
+                const cursor = await productCollection.find(properties)
+                resolve(cursor.toArray())
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+product.update = (filters, values, options, operator) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let opr = '$set'
+            switch(operator){
+                case 'unset':
+                    opr = '$unset'
+                    break
+                case 'rename':
+                    opr = '$rename'
+                    break
+                case 'set':
+                    break
+                default:
+                    if (validator.isNotNull(operator)) {
+                        let e = new Error('Invalid product operation')
+                        e.name = 'ProductError'
+                        e.type = 'Invalid Operation'
+                        throw e
+                    }
+            }
+            const productCollection = db.get().collection('products')
+            let operation = {}
+            operation[opr] = values
+            const result = await productCollection.updateMany(filters,operation,options)
+            resolve(result)
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+product.delete = (filters) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const productCollection = db.get().collection('products')
+            const result = await productCollection.deleteMany(filters)
+            resolve(result)
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+module.exports = product
--- models/schema.md
+++ models/schema.md
@@ -1,14 +1,152 @@
+##Sample User Model (JSON):
+```
+    {
+        "_id": ObjectId("2342345235"),
+        "displayName": "Mork Hashimoto",
+        "name": {
+            "familyName": "Hashimoto",
+            "givenName": "Mork"
+        },
+        "birthday": "0000-01-16",
+        "gender": "male",
+        "tags": [
+            "plaxo guy"
+        ],
+        "emails": [
+            {
+            "value": "mhashimoto-04@plaxo.com",
+            "type": "work",
+            "primary": "true"
+            },
+            {
+            "value": "mhashimoto-04@plaxo.com",
+            "type": "home"
+            },
+            {
+            "value": "mhashimoto@plaxo.com",
+            "type": "home"
+            }
+        ],
+        "urls": [
+            {
+            "value": "http://www.seeyellow.com",
+            "type": "work"
+            },
+            {
+            "value": "http://www.angryalien.com",
+            "type": "home"
+            }
+        ],
+        "phoneNumbers": [
+            {
+            "value": "KLONDIKE5",
+            "type": "work"
+            },
+            {
+            "value": "650-123-4567",
+            "type": "mobile"
+            }
+        ],
+        "photos": [
+            {
+            "value": "http://sample.site.org/photos/12345.jpg",
+            "type": "thumbnail"
+            }
+        ],
+        "ims": [
+            {
+            "value": "plaxodev8",
+            "type": "aim"
+            }
+        ],
+        "addresses": [
+            {
+            "type": "home",
+            "streetAddress": "742 Evergreen Terrace\nSuite 123",
+            "locality": "Springfield",
+            "region": "VT",
+            "postalCode": "12345",
+            "country": "USA",
+            "formatted":
+            "742 Evergreen Terrace\nSuite 123\nSpringfield, VT 12345 USA"
+            }
+        ],
+        "organizations": [
+            {
+            "name": "Burns Worldwide",
+            "title": "Head Bee Guy"
+            }
+        ],
+        "accounts": [
+            {
+            "domain": "plaxo.com",
+            "userid": "2706"
+            }
+        ],
+        roles: []
+    }
+```
+##Sample Shop Model (JSON): 
+```
+    {
+        "_id": ObjectId("2342345235"),
+        "owner": "",
+        "name": "",
+        "displayName": "",
+        "description": "",
+        "addresses": [
+            {
+                "streetAddress": "",
+                "locality": "",
+                "region": "",
+                "postalCode": "",
+                "country": ""
+            }
+        ],
+        "phoneNumbers": [
+            {
+                "type": "",
+                "value": ""
+            }
+        ],
+        "catalogs": [""]
+    }
+```
+##Sample Product Model (JSON):
+```
+    {
+        "_id": ObjectId("2342345235"),
+        "shop": "",
+        "parentProduct": "",
+        "name": "",
+        "description": "",
+        specifications: {
+
+        },
+        "isSKU": true,
+        "SKU": "",
+        "price", 00.00,
+        "currency": "",
+        "images": [
+            {
+            "value": "http://sample.site.org/photos/12345.jpg",
+            "type": "thumbnail"
+            }
+        ]
+    }
+```
+##Sample Catalog Model (JSON):
+```
+    {
+        "_id":, ObjectId("2342345235"),
+        "name": "",
+        "description": "",
+        "products": [""]
+    }
+```
+
 Currency:
 Fields - id, code
 
-Product:
-Fields - _id, shop, parentProduct, name, description, specifications, isSKU, SKU, price, currency, image
-
-Catalog:
-Fields - _id, name, description, productListing: []
-
 Order:
 Fields - _id, timestamp, user, product, quantity
-
-Shop:
-Fields - _id, owner, name, displayName, description, address: {streetAddress, locality, region, postalCode, country}, phoneNumber: {value,type}

e8dca81f89c27aef576ef2a59e6ec1363594161f
Nneko Branche <4615594+nneko@users.noreply.github.com>
2020-09-15 20:22:57 -0500

    product post and user/bag/checkout skeleton

--- controllers/market/product.js
+++ controllers/market/product.js
@@ -1,6 +1,7 @@
 const cfg = require('../../configuration')
 const express = require('express')
 const validator = require('../../utilities/validator')
+const converter = require('../../utilities/converter')
 const handler = require('../handlers/market/product')
 const debug = cfg.env == 'development' ? true : false
 
@@ -18,6 +19,27 @@ marketProduct.get('/', async (req, res) => {
         res.status(500)
         res.render('error', { error: { status: 500, message: 'Error retrieving data' }, name: '', user: req.user })
     }
+})
+
+marketProduct.post('/', async (req, res) => {
+    try {
+        let form = converter.objectFieldsToString(req.body)
+        if (validator.isNotNull(form)) {
+            let viewData = await handler.populateViewData(form.pid)
+            viewData.user = req.user
+            viewData.messages = {info: 'Feature not implemented'}
+            res.status(501)
+            res.render('product', viewData)
+        } else {
+            res.status(400)
+            res.redirect('/market')
+        }
+    } catch (e) {
+        console.error(e)
+        res.status(500)
+        res.render('error', { error: { status: 500, message: 'Error retrieving data' }, name: '', user: req.user })
+    }
+
 })
 
 module.exports = marketProduct
---
+++ controllers/user/bag/checkout.js
@@ -0,0 +1,42 @@
+const cfg = require('../../../configuration')
+const express = require('express')
+const validator = require('../../../utilities/validator')
+const converter = require('../../../utilities/converter')
+const handler = require('../../handlers/market')
+const debug = cfg.env == 'development' ? true : false
+
+let checkout = express.Router()
+
+checkout.get('/', async (req, res) => {
+    try {
+        let viewData = await handler.populateViewData(req.user)
+        viewData.user = req.user
+        viewData.messages = {info: 'Feature not implemented.'}
+        res.status(501)
+        res.render('market', viewData)
+    } catch (e) {
+        console.error(e)
+        res.status(500)
+        res.render('error', { error: { status: 500, message: 'Error retrieving data' }, name: '', user: req.user })
+    }
+})
+
+checkout.post('/', async (req, res) => {
+    try {
+        let form = converter.objectFieldsToString(req.body)
+        if (debug) console.log(form)
+
+        let viewData = await handler.populateViewData(req.user)
+        viewData.user = req.user
+        viewData.messages = { info: 'Feature not implemented' }
+        res.status(501)
+        res.render('market', viewData)
+    } catch (e) {
+        console.error(e)
+        res.status(500)
+        res.render('error', { error: { status: 500, message: 'Error retrieving data' }, name: '', user: req.user })
+    }
+
+})
+
+module.exports = checkout

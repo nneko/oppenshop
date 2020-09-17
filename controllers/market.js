306c0aff4969646d3ff48827a3ab596af006932b
Nneko Branche <4615594+nneko@users.noreply.github.com>
2020-09-16 21:07:24 -0500

    guest users can browse market pages

--- controllers/market.js
+++ controllers/market.js
@@ -11,22 +11,34 @@ market.use('/seller*', require('./market/seller'))
 market.use('/product*', require('./market/product'))
 market.use('/page/:page', async function (req, res, next) {
     try {
+        let page = req.params.page || 1
+        let qd = req.query
+        console.log(page)
+        console.log(qd)
+        let viewData = {}
+        console.log('Page: ' + page)
+        viewData = await handler.populateViewData(validator.isNotNull(req.user) ? req.user.id : null, product_page = parseInt(page))
+        viewData.user = req.user
+        res.render('market', viewData)
+    } catch (e) {
+        console.error(e)
+        res.status(500)
+        res.render('error', { error: { status: 500, message: 'Error retrieving shop pagination data' }, name: '', user: req.user })
 
-        if (validator.hasActiveSession(req)) {
-            let page = req.params.page || 1
-            let qd = req.query
-            console.log(page)
-            console.log(qd)
-            let viewData = {}
-            console.log('Page: ' + page)
-            viewData = await handler.populateViewData(req.user.id.toString(), product_page = parseInt(page))
-            viewData.user = req.user
-            res.render('market', viewData)
-        } else {
-            messages = { error: "You need to be signed in." }
-            res.status(403)
-            res.render('signin', { messages: messages })
-        }
+    }
+})
+
+market.use('/shops/page/:page', async function (req, res, next) {
+    try {
+        let page = req.params.page || 1
+        let qd = req.query
+        console.log(page)
+        console.log(qd)
+        let viewData = {}
+        console.log('Page: ' + page)
+        viewData = await handler.populateViewData(validator.isNotNull(req.user) ? req.user.id : null, product_page = parseInt(page))
+        viewData.user = req.user
+        res.render('market', viewData)
     } catch (e) {
         console.error(e)
         res.status(500)

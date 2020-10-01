a26e3e48f95f5f4b01a31577de5d76aed82423c1
Nneko Branche <4615594+nneko@users.noreply.github.com>
2020-10-01 16:22:52 -0500

    Admin controllers

---
+++ controllers/admin.js
@@ -0,0 +1,43 @@
+const cfg = require('../configuration')
+const validator = require('../utilities/validator')
+const converter = require('../utilities/converter')
+const media = require('../adapters/storage/media')
+const user = require('../models/user')
+const express = require('express')
+const debug = cfg.env == 'development' ? true : false
+
+let admin = express.Router()
+
+admin.get('/', async (req, res) => {
+    try {
+        let viewData = {}
+        viewData.messages = {info: 'Functionality not implemented.'}
+        viewData.user = req.user
+        res.status = 501
+        res.render('admin', viewData)
+
+    } catch (e) {
+        console.error(e)
+        res.status(500)
+        res.render('error', { error: { status: 500, message: 'Error retrieving data' }, name: '', user: req.user })
+    }
+})
+
+admin.post('/', async (req, res) => {
+    try {
+        let form = converter.objectFieldsToString(req.body)
+        if (debug) console.log(form)
+
+        let viewData = {}
+        viewData.user = req.user
+        viewData.messages = { info: 'Feature not implemented' }
+        res.status(501)
+        res.render('admin', viewData)
+    } catch (e) {
+        console.error(e)
+        res.status(500)
+        res.render('error', { error: { status: 500, message: 'Error retrieving data' }, name: '', user: req.user })
+    }
+})
+
+module.exports = admin
